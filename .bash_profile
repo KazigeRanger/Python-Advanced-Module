a23c52f444cd7b33ed14325ece6132ee4a0b9b93
Renger Bierenga <bierengarenger@gmail.com>
2024-09-26 19:37:17 +0200

    Created a .bash_profile

---
+++ .bash_profile
@@ -0,0 +1,6 @@
+alias part1="cd \"Deel 1 - Lineaire Algebra\""
+alias run11="python3 Opdr1LinAlg.py"
+alias run12="python3 Opdr2LinAlg.py"
+alias run13="python3 Opdr3LinAlg.py"
+alias run14="python3 Opdr4LinAlg.py"
+alias run15="python3 Opdr5LinAlg.py"
--- Deel 1 - Lineaire Algebra/Opdr4LinAlg.py
+++ Deel 1 - Lineaire Algebra/Opdr4LinAlg.py
@@ -14,12 +14,14 @@ prijzen_per_stuk = np.array([0.5, 0.3, 0.7, 1.2])
 
 # functie die de berekening kan uitvoeren
 def bereken_totaalprijs(aantallen, prijzen):
-    for i in range(len(aantallen)):
-        print(str(aantallen[i]) + " * " + str(prijzen) + " = " + str(np.dot(aantallen[i], prijzen)))
-    
-    print("Matrix dot product is: " + str(np.dot(aantallen, prijzen)))
-    print("Output array element 4: " + str(np.dot(aantallen, prijzen)[3]))
+    # emptyList = []
+    # dotProduct = np.dot(aantallen, prijzen)
+    # for i in range(len(dotProduct)):
+    #     emptyList.append(np.float64(dotProduct[i]))
+    #     print(str(dotProduct[i]) + " " + str(type(dotProduct[i])))
+    # return emptyList
     return np.dot(aantallen, prijzen)
+
 
 totaalprijs_per_persoon = bereken_totaalprijs(aantallen_fruit, prijzen_per_stuk)
 print(totaalprijs_per_persoon)
